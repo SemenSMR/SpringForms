e8cab00f748967198c7de50c9d87f5ed5f5cac1a
Evgeniy <svorovali@mail.ru>
2024-01-08 10:53:14 +0300

    Init File

---
+++ src/main/java/org/example/ResponseUtils.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ResponseUtils {
+
+    public static void responseOk(Path filePath, byte[] content, BufferedOutputStream out) {
+        try {
+            String mimeType = Files.probeContentType(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.write(content);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,41 @@
+package org.example;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final ExecutorService pool = Executors.newFixedThreadPool(64);
+    private final Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
+
+    public void listen(int port) {
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (
+                        final var socket = serverSocket.accept()
+                ) {
+                    var connectListener = new ConnectListener(socket, handlers);
+                    pool.submit(connectListener).get();
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        Map<String, Handler> map = new ConcurrentHashMap<>();
+        if (handlers.containsKey(method)) {
+            map = handlers.get(method);
+        }
+        map.put(path, handler);
+        handlers.put(method, map);
+    }
+}
